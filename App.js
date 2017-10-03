253de81af6f7adb6758b051d56421e5a4c3994c8
Rafael Felipe <rafafelipesilva@gmail.com>
2017-10-03 18:06:24 -0300

    Design FormCadastro

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormLogin from './src/components/FormLogin';
+import FormCadastro from './src/components/FormCadastro';
 
 export default props => (
-  <FormLogin />
+  <FormCadastro />
 );
