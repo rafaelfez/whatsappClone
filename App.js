c2ff3082428592032dcd8ea96e988a4b6445fd29
Rafael Felipe <rafafelipesilva@gmail.com>
2017-10-04 13:05:38 -0300

    Instalando Redux e combinando Reducers

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import Routes from './src/Routes';
+import reducers from './src/reducers/'; //index importa automaticamente
 
 export default props => (
-  <Routes />
+  <Provider store={createStore(reducers)}>
+    <Routes />
+  </Provider>
 );
