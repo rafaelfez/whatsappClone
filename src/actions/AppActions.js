c57ddc95346d39cf63d6735f2eba580c1b62e9d4
Rafael Felipe <rafafelipesilva@gmail.com>
2017-10-21 13:24:45 -0200

    AdicionarContato +

---
+++ src/actions/AppActions.js
@@ -0,0 +1,75 @@
+import b64 from 'base-64';
+import firebase from 'firebase';
+import _ from 'lodash';
+
+import { 
+    MODIFICA_ADICIONA_CONTATO_EMAIL,
+    ADICIONA_CONTATO_ERRO,
+    ADICIONA_CONTATO_SUCESSO 
+} from './types';
+
+export const modificaAdicionaContatoEmail = texto => {
+    console.log('chegamos aqui');
+    console.log(texto);
+    return {
+        type: MODIFICA_ADICIONA_CONTATO_EMAIL,
+        payload: texto
+    }
+}
+
+export const adicionaContato = email => {
+
+    return dispatch => {
+        let emailB64 = b64.encode(email);
+
+        firebase.database().ref(`/contatos/${emailB64}`)
+            .once('value')
+            .then(snapshot => {
+                if(snapshot.val()) {
+                    //email do contato que adicionaremos
+                    const dadosUsuario = _.first(_.values(snapshot.val()));
+                    console.log(dadosUsuario);
+                    //email do usuario autenticado
+                    const { currentUser } = firebase.auth();
+                    let emailUsuarioB64 = b64.encode(currentUser.email);
+
+                    firebase.database().ref(`/usuario_contatos/${emailUsuarioB64}`)
+                        .push({ email, nome: dadosUsuario.nome })
+                        .then(() => adicionaContatoSucesso(dispatch))
+                        .catch(erro => adicionaContatoErro(erro.message, dispatch))
+                } else {
+                    dispatch(
+                        { 
+                            type: ADICIONA_CONTATO_ERRO,
+                            payload: 'E-mail informado não corresponde a um usuário válido'
+                        }
+                    )
+                }
+            })
+    }
+}
+
+const adicionaContatoErro = (erro, dispatch) => (
+    dispatch (
+        {
+            type: ADICIONA_CONTATO_ERRO,
+            payload: erro
+        }
+    )
+)
+
+const adicionaContatoSucesso = dispatch => {
+    dispatch (
+        {
+            type: ADICIONA_CONTATO_SUCESSO,
+            payload: true
+        }
+    )
+}
+
+export const habilitaInclusaoContato = () => (
+    {
+        type: ADICIONA_CONTATO_SUCESSO,
+        payload: false
+    }
+)
